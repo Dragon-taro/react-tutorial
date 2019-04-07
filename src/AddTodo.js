f56ca550e50122cf7b699ea58a59bd9aad0644f6
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-04-08 00:39:57 +0900

    レガシーコードの復活

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 
-class AddTodo extends Component {
+class AddTodo extends React.Component {
+  // レガシー
+  // onChange(e) {
+  //   const key_value = {
+  //     value: e.target.value
+  //   };
+  //   this.props.onChange(key_value);
+  // }
+
   onChange(e) {
     const { onChange } = this.props;
     onChange({
       value: e.target.value
     });
   }
+
+  // レガシー
+  // add() {
+  //   if (this.props.value) {
+  //     const todoElement = {
+  //       content: this.props.value,
+  //       id: this.props.todoList.length + 1
+  //     };
+  //     this.props.add(todoElement);
+  //   }
+  // }
 
   add() {
     const { value, todoList, add } = this.props;
@@ -18,6 +37,20 @@ class AddTodo extends Component {
       add(todoElement);
     }
   }
+
+  // レガシー
+  // render() {
+  //   return (
+  //     <div>
+  //       <input
+  //         type="text"
+  //         value={this.props.value}
+  //         onChange={this.onChange.bind(this)}
+  //       />
+  //       <button onClick={this.add.bind(this)}>追加</button>
+  //     </div>
+  //   );
+  // }
 
   render() {
     const { value } = this.props;
